f40949d5c4fa870c90018cbea655d08e9432f6a8
Jovincia <assogbadanielle15@gmail.com>
2025-05-11 23:52:05 +0100

    deployment of the app

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4256849854"
+    serviceWorkerVersion: "2137460993"
   }
 });
